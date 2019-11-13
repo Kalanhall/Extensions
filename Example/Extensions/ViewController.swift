bc1a515e177f80fec5cf1998d66ec60df3c31b26
Kimee <wujm002@galanz.com>
2019-11-13 18:57:24 +0800

    first init.

--- Example/Extensions/ViewController.swift
+++ Example/Extensions/ViewController.swift
@@ -7,12 +7,16 @@
 //
 
 import UIKit
+import Extensions
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        UIImage.image(named: "", in: Bundle(for: type(of: self)))?.original
+        UIImage.image(color: UIColor.black)
     }
 
     override func didReceiveMemoryWarning() {
